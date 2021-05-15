5a32c8743a2eb9011bbff0cc43a8b473d5446661
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-05-15 17:34:19 +0800

    Update street.py

--- AIO/street construction/street.py
+++ AIO/street construction/street.py
@@ -2,31 +2,11 @@ infile=open('streetin.txt','r')
 outfile=open('streetout.txt','w')
 n,k=map(int,infile.readline().split())
 
-if n==17 and k==4:
-    answer=3
-elif k==1:
-    answer=n//2
-elif k==2:
-    if n==2:
-        answer=0
-    elif n==3 or n==4:
-        answer=1
-    else:
-        n-=2
-        if n%3!=0:
-            answer=(n//3)+1
-        else:
-            answer=n//3
-elif k==0:
-    answer=n
-elif n==k:
-    answer=0
+n-=k
+if n%(k+1)!=0:
+    answer=(n//(k+1))+1
 else:
-    n-=k
-    if n%(k+1)!=0:
-        answer=(n//(k+1))+1
-    else:
-        answer=n//(k+1)
+    answer=n//(k+1)
 
 outfile.write(str(answer))
 outfile.close()
