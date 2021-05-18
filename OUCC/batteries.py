5c8aa58114e298f446d637b2795929150ef35dce
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-05-18 15:18:39 +0800

    Update batteries.py

--- OUCC/batteries.py
+++ OUCC/batteries.py
@@ -1,26 +1,13 @@
-times=int(input())
-n=0
-list1=[]
-for i in range(times):
-    c=list(map(int,input().split(',')))
-    list1.append(c)
-for i in range(len(list1)):
-    m,d=list1[i][0],list1[i][1]''
-    if d<0:
-        for e in range(i,len(list1)):
-            if list1[e][1]<0:
-                p=True
-            else:
-                p=False
-    if d<0 and p==True:
-        break ''
-            
-    if d==0:
-        n+=m
-    elif d>0:
-        n+=m+d//10
-    elif d<0:
-        n+=(d//10)*m
-    if n<0:
-        n=0
-print(n)
+p=int(input())
+l=[0]
+used=0
+for i in range(p):
+    mins,incline=map(int,input().split(','))
+    if incline>0:
+        used=used+1*mins+incline//10
+    elif incline==0:
+        used=used+1*mins
+    else:
+        used=max(0,used+(incline//10)*mins)
+    l.append(used)
+print(max(l))
