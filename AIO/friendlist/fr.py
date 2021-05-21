50b2e6a96162046790135193c9f3b87eb4ee7773
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-05-21 15:02:09 +0800

    Update fr.py

--- AIO/friendlist/fr.py
+++ AIO/friendlist/fr.py
@@ -1,32 +1,25 @@
 infile=open('listin.txt','r')
-f=int(infile.readline().strip())
+times=int(infile.readline())
+list1=[0]*1001
 
-list1=[]
-list2=[]
-list3=[]
-for i in range(f):
-    a,b=map(int,infile.readline().strip().split())
-    list1.extend((a,b))
+for i in range(times):
+    a,b=map(int,infile.readline().split())
+    list1[a]+=1
+    list1[b]+=1
 
-for i in list1:
-    if i not in list2:
-        list2.append(i)
-
-max1=list1.count(list2[0])
-list3.append(max1)
-for i in list2[1:]:
-    if list1.count(i)>max1:
-        list3.clear()
-        list3.append(i)
-        max1=list1.count(i)
-    elif list1.count(i)==max1:
-        list3.append(i)
-
-list3.sort()
-number=''
-for i in list3:
-    number+=str(i)+'\n'
+max1=0
+answer=[]
+for i in range(len(list1)):
+    if list1[i]<max1:
+        pass
+    elif list1[i]==max1:
+        answer.append(i)
+    else:
+        answer=[]
+        answer.append(i)
+        max1=list1[i]
 
 outfile=open('listout.txt','w')
-outfile.write(str(number))
+for i in answer:
+    outfile.write(str(i)+'\n')
 outfile.close()
