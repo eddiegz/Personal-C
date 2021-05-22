6655917f6b244341199a73c71dcf442707f3c371
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-05-22 16:01:23 +0800

    Update a mindbending scenario.py

--- AIO/a mindbending scenario/a mindbending scenario.py
+++ AIO/a mindbending scenario/a mindbending scenario.py
@@ -1,24 +1,21 @@
 infile=open('bendin.txt','r')
 axl,ayl,axr,ayr=map(int,infile.readline().strip().split())
 bxl,byl,bxr,byr=map(int,infile.readline().strip().split())
-areaa=(axr-axl)*(ayr-ayl)
-areab=(bxr-bxl)*(byr-byl)
-answer=areaa+areab
 
-if axr>=bxl and ayr>=byl:
-    areai=(axr-bxl)*(ayr-byl)
-    answer-=areai
-elif bxl>=axr and byr>=ayl:
-    areai=(bxl-axr)*(byr-ayl)
-    answer-=areai
-elif axl>=bxr and ayr>=byl:
-    areai=(axl-bxr)*(ayr-byl)
-    answer-=areai
-elif bxr>=axl and byr>=ayl:
-    areai=(bxr-axl)*(byr-ayl)
-    answer-=areai
+left=max(axl,bxl)
+bottom=max(ayl,byl)
+right=min(axr,bxr)
+top=min(ayr,byr)
+area1=(axr-axl)*(ayr-ayl)
+area2=(bxr-bxl)*(byr-byl)
+if(left<right and bottom<top):
+    areai=(right-left)*(top-bottom)
+else:
+    areai=0
 
+answer=area1+area2-areai
 
 outfile=open('bendout.txt','w')
 outfile.write(str(answer))
 outfile.close()
+
