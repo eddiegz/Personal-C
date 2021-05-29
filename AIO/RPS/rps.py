0a58c56769fdebf137dab57b6976fd353c190533
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-05-29 10:58:43 +0800

    Update rps.py

--- AIO/RPS/rps.py
+++ AIO/RPS/rps.py
@@ -1,102 +1,29 @@
 infile=open('rpsin.txt','r')
+outfile=open('rpsout.txt','w')
 n=int(infile.readline())
 ra,pa,sa=map(int,infile.readline().split())
 rb,pb,sb=map(int,infile.readline().split())
-answer=0
-leftra,leftrb,leftsa,leftsb,leftpa,leftpb=0,0,0,0,0,0
-'''
-if ra<pb:
-    answer+=ra
-elif pb==0:
-    answer+=pb-ra
-else:
-    answer+=pb
-if pa<sb:
-    answer+=pa
-elif sb==0:
-    answer+=sb-pa
-else:
-    answer+=sb
-if sa<rb:
-    answer+=sa
-elif rb==0:
-    answer+=rb-sa
-else:
-    answer+=rb'''
-if pb==0 and sb==0 and rb>0:
-    answer+=sa
-    answer-=pa
-elif sa==0 and sb==0:
-    if pa==pb:
-        answer=0
-    elif pa<pb:
-        answer=pb-pa
-    elif pa>pb:
-        answer=ra-rb
-elif ra>0 and pa==0 and sa==0:
-    answer-=sb
-    answer+=pb
-elif rb==1:
-    if ra==1:
-        answer=0
-    elif pa==1:
-        answer=-1
-    elif sa==1:
-        answer=1
-elif pb==1:
-    if ra==1:
-        answer=1
-    elif pa==1:
-        answer=0
-    elif sa==1:
-        answer=-1
-elif sb==1:
-    if ra==1:
-        answer=-1
-    elif pa==1:
-        answer=1
-    elif sa==1:
-        answer=0
-else:
-    a=ra-pb
-    if a==0:
-        answer+=ra
-    elif a>0:
-        answer+=pb
-        leftra=a
-    elif a<0:
-        answer+=pb
-        leftpb=abs(a)
-    b=pa-sb
-    if b==0:
-        answer+=pa
-    elif b>0:
-        answer+=sb
-        leftrb=b
-    elif b<0:
-        leftsb=abs(b)
-    c=sa-rb
-    if c==0:
-        answer+=sa
-    elif c>0:
-        answer+=rb
-        leftsa=c
-    elif a<0:
-        answer+=rb
-        leftrb=abs(c)
-    if leftpb>0:
-        lb=leftpb-leftpa
-    else:
-        la=leftra-leftrb
-    if leftrb>0:
-        lr=leftrb-leftra
 
-        
-    
-        
-    
+win=min(rb,sa)+min(pb,ra)+min(sb,pa)
+if rb>sa:
+    rb,sa=rb-sa,0
+elif rb==sa:
+    rb,sa=0,0
+else:
+    sa,rb=sa-rb,0
+if pb>ra:
+    pb,ra=pb-ra,0
+elif pb==ra:
+    pb,ra=0,0
+else:
+    ra,pb=ra-pb,0
+if sb>pa:
+    sb,pa=sb-pa,0
+elif sb==pa:
+    sb,pa=0,0
+else:
+    pa,sb=pa-sb,0
 
-
-outfile=open('rpsout.txt','w')
-outfile.write(str(answer))
+outfile.write(str(win-(max(0,rb-ra)+max(0,pb-pa)+max(0,sb-sa))))
 outfile.close()
+
