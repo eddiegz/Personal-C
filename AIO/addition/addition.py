86cc81384033fdaf5b723ddab02b4978e5b726d4
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-06-09 08:24:02 +0800

    Update addition.py

--- AIO/addition/addition.py
+++ AIO/addition/addition.py
@@ -1,7 +1,1 @@
-a,b = map(int,open("addin.txt", "r").readline().strip().split())
-
-answer = a+b
-
-outfile = open("addout.txt", "w")
-outfile.write(str(answer))
-outfile.close()
+open('addout.txt','w').write(str(sum(map(int,open('addin.txt','r').readline().split()))))
