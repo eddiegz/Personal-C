09c3c0573cae5764f30561de91af04e849d89156
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-03 23:58:34 +0800

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vvi = vector<vi>;
+#define pb push_back
+#define pob pop_back
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<string,int>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define pii pair<int,int>
+#define psi pair<string,int>
+#define pss pair<string,string>
+#define pci pair<char,int>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+
+const int MOD=1e9+7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    freopen("example.in","r",stdin);
+    freopen("example.out","w",stdout);
+}
