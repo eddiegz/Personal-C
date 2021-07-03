c26291a723fa8b5743216e38ab434eff820d93b2
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-04 00:15:03 +0800

    Add files via upload

---
+++ FARIO/bookshop.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vvi = vector<vi>;
+#define pb push_back
+#define pob pop_back
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<string,int>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define pii pair<int,int>
+#define psi pair<string,int>
+#define pss pair<string,string>
+#define pci pair<char,int>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+
+const int MOD=1e9+7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;cin>>n;int arr[n+1];
+    for(int i=1;i<n+1;++i){cin>>arr[i];}
+    int k;cin>>k; int index=1;
+    while(k--){
+        int b;cin>>b;
+        while(arr[index]!=b){
+            index++;
+        }
+        cout<<index<<endl;
+    }
+}
