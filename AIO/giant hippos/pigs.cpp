6a3d79f59815be3a0f14c1e2c0938f27d51bf81d
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-04 17:49:59 +0800

    Add files via upload

---
+++ AIO/giant hippos/pigs.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vvi = vector<vi>;
+#define pb push_back
+#define pob pop_back
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<string,int>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define pii pair<int,int>
+#define psi pair<string,int>
+#define pss pair<string,string>
+#define pci pair<char,int>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+
+const int MOD=1e9+7;
+
+int main() {
+    freopen("hippoin.txt","r",stdin);
+    freopen("hippoout.txt","w",stdout);
+	int n,h,f;
+	int sum = 0, max = 0, maxid;
+	scanf("%d %d %d",&n, &h, &f);
+	int e[h], d[h + 1];
+	int init=f;
+	for(int i=0; i<h; i++) {scanf("%d", &e[i]);}
+	d[0]=e[0]-1;
+	for(int i=1; i<h; i++){d[i] = e[i]-e[i-1]-1;}
+	d[h]=n-e[h-1];
+	bool done=false;
+	if(f%2 == 1) {
+			if(d[h]>d[0]) {sum+=d[h]; d[h]=0;}
+			else {sum+=d[0];d[0]=0;}
+			f--;
+		}
+	while(f>0 && f>init-2*h){
+		max=0;
+		for(int i=0; i<=h; i++){if(d[i]>max){max=d[i]; maxid=i;}}
+		
+		if(f>1){
+			if(d[h]>=max){f--; sum+=d[h]; d[h]=0;}
+			else if(d[0]>=max){f--; sum+=d[0]; d[0]=0;}
+			else if(d[0]+d[h]>=max){f-=2; sum+=d[0]+d[h]; d[h]=0; d[0]=0;}
+			else{sum+=d[maxid]; f-=2; d[maxid]=0;}
+		}
+		if(f == 1) {
+			if(d[h]>d[0]) {sum+=d[h];}
+			else {sum+=d[0];}
+			f=0;
+		}
+	}
+	printf("%d", sum);
+}
---
+++ AIO/giant hippos/test.py
@@ -0,0 +1,27 @@
+infile=open('hippoin.txt','r').readlines()
+n,h,f=map(int,infile[0].strip().split())
+place=[0]+[int(i) for i in infile[1:]]+[n]
+length=[]
+for i in range(1,h+1):
+    length.append(place[i]-place[i-1]-1)
+length.append(place[-1]-place[-2])
+answer=0
+modi=length[1:-1]
+modi.sort(reverse=True)
+    
+chance=f//2
+if chance>0:answer=sum(modi[:chance])
+
+chance=(f-1)//2
+current=0
+if length[0]>length[-1]:current+=length[0]
+else:current+=length[-1]
+if chance>0:current+=sum(modi[:chance])
+answer=max(current,answer)
+
+chance=(f-2)//2
+current=length[0]+length[-1]
+if chance>0:current+=sum(modi[:chance])
+answer=max(current,answer)
+
+open('hippoout.txt','w').write(str(max(0,answer)))
