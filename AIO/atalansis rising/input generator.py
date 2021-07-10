3b20848512bdbfc61f15f1b116142f87d3d84a50
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-10 17:13:08 +0800

    Delete input generator.py

--- AIO/atalansis rising/input generator.py
+++
@@ -1,9 +0,0 @@
-#input generator
-import random
-outfile=open('atlanin.txt','w')
-times=int(input())
-outfile.write(str(times)+'\n')
-for i in range(times):
-    outfile.write(str(random.randint(1,2000000000))+'\n')
-
-outfile.close()
