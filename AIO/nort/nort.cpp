2107ec65a9905d7620c7d007d50a58f3d62805a4
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-16 00:32:32 +0800

    Add files via upload

---
+++ AIO/nort/nort.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vc = vector<char>;
+using vvi = vector<vi>;
+using vvc = vector<vc>;
+#define pb push_back
+#define pob pop_back
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<str,int>
+#define mis map<int,str>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define mll map<ll,ll>
+#define pii pair<int,int>
+#define pis pair<int,str>
+#define psi pair<str,int>
+#define pss pair<str,str>
+#define pci pair<char,int>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define rep( i , s , e ) for(int i = s ; i <= e ; ++i)
+#define repR( i , e , s ) for(int i = e ; i >= s ; --i)
+
+void inoutio(string s){
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void txtio(str inf,str ouf){
+    freopen(inf.c_str(), "r" , stdin);
+    freopen(ouf.c_str(), "w" , stdout);
+}
+
+const int MOD=1e9+7;
+const int MAXN=1e6;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    txtio("nortin.txt","nortout.txt");
+    int w,h;
+    cin>>w>>h;
+    if(w%2==0 || h%2==0)cout<<w*h<<endl;
+    else cout<<w*h-1<<endl;
+}
