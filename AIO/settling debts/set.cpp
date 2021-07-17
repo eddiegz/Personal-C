a00ef9dda22a783b58c7fe5c5c0e7e3e79659c4c
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-17 17:47:00 +0800

    Add files via upload

---
+++ AIO/settling debts/set.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vc = vector<char>;
+using vvi = vector<vi>;
+using vvc = vector<vc>;
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<str,int>
+#define mis map<int,str>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define mll map<ll,ll>
+#define pii pair<int,int>
+#define pis pair<int,str>
+#define psi pair<str,int>
+#define pss pair<str,str>
+#define pci pair<char,int>
+#define pcc pair<char,char>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define rep( i , s , e ) for(int i = s ; i <= e ; ++i)
+#define repR( i , e , s ) for(int i = e ; i >= s ; --i)
+
+void inoutio(str s){
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void txtio(str inf,str ouf){
+    freopen(inf.c_str(), "r" , stdin);
+    freopen(ouf.c_str(), "w" , stdout);
+}
+
+const int MOD=1e9+7;
+const int MAXN=1e6;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    txtio("debtsin.txt","debtsout.txt");
+    int n,total=0,id,count=0;
+    cin>>n;
+    int arr[n];
+    int current=n;
+    rep(i,0,n-1){cin>>arr[i];}
+    rep(i,0,n-1){
+        total=arr[i];count++;id=1;
+        while(total<=0 && current==n){
+            total+=arr[(id+i)%n];id++;
+            if(total==0 && id>n-2){
+                current=i;
+                cout<<current+1<<endl;
+                return 0;
+            }
+        }
+        i=(i+id)%n-1;
+    }
+}
