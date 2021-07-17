38af2d8ec79bdf5f259e7f40143a380b9b3be190
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-17 22:05:23 +0800

    Update ghost.cpp

--- AIO/ghost encounters/ghost.cpp
+++ AIO/ghost encounters/ghost.cpp
@@ -1,23 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vc = vector<char>;
+using vvi = vector<vi>;
+using vvc = vector<vc>;
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<str,int>
+#define mis map<int,str>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define mll map<ll,ll>
+#define pii pair<int,int>
+#define pis pair<int,str>
+#define psi pair<str,int>
+#define pss pair<str,str>
+#define pci pair<char,int>
+#define pcc pair<char,char>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define rep( i , s , e ) for(int i = s ; i <= e ; ++i)
+#define repR( i , e , s ) for(int i = e ; i >= s ; --i)
+
+void inoutio(str s){
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void txtio(str inf,str ouf){
+    freopen(inf.c_str(), "r" , stdin);
+    freopen(ouf.c_str(), "w" , stdout);
+}
+
+const int MOD=1e9+7;
+const int MAXN=1e5+5;
 
 int main(){
-    freopen("ghostin.txt","r",stdin);
-    freopen("ghostout.txt","w",stdout);
-    map<int,int> m;
-    ll n,k,x,t;
-    scanf("%lli%lli",&n,&k);
-    for (int i=0;i<n;++i){
-        scanf("%lli%lli",&x,&t);
-        if (m.count(x)){
-            m[x]+=t;
-        }
-        else{
-            m[x]=t;
-        }
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    txtio("ghostin.txt","ghostout.txt");
+    int n,k,x[MAXN],time[MAXN],y[MAXN],ans=0;cin>>n>>k;
+    mii dic;
+    rep(i,1,n){cin>>x[i]>>y[i];}
+    rep(i,1,n){time[i]=x[i]*k;}
+    rep(i,1,n){
+        int ts=y[i]-time[i];
+        if(dic.count(ts)==false){dic[ts]=0;}
+        dic[ts]++;
     }
-    for (auto x:m){
-        cout<<x.first<<" "<<x.second<<"\n";
-    }
+    for(auto ele:dic){ans=max(ans,ele.ss);}
+    cout<<ans<<endl;
 }
