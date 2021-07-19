1a84b030ba4883cdfad1b7ce9161311ecedae7ad
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-19 19:13:36 +0800

    Update queues.cpp

--- AIO/queues/queues.cpp
+++ AIO/queues/queues.cpp
@@ -1,31 +1,93 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vc = vector<char>;
+using vvi = vector<vi>;
+using vvc = vector<vc>;
+#define pb push_back
+#define pob pop_back
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<str,int>
+#define mis map<int,str>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define mll map<ll,ll>
+#define pii pair<int,int>
+#define pis pair<int,str>
+#define psi pair<str,int>
+#define pss pair<str,str>
+#define pci pair<char,int>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define rep( i , s , e ) for(int i = s ; i <= e ; ++i)
+#define repR( i , e , s ) for(int i = e ; i >= s ; --i)
 
-void print(queue<int> q){
-    queue<int> copy=q;
-    while (!copy.empty()){
-        printf("%d",q.front());
-        copy.pop();
+void inoutio(string s){
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void txtio(str inf,str ouf){
+    freopen(inf.c_str(), "r" , stdin);
+    freopen(ouf.c_str(), "w" , stdout);
+}
+
+const int MOD=1e9+7;
+const int MAXN=1e3;
+
+int qa[MAXN];
+int qf,qb;
+
+void qpu(int value){
+    qa[qb%MAXN]=value;
+    qb++;
+}
+
+int qpo(){
+    int retval=qa[qf%MAXN];
+    qf++;
+    return retval;
+}
+
+bool qe(){
+    return(qb==qf);
+}
+
+void print(){
+    if(qe()){
+        cout<<"queue is empty!"<<endl;
+    }
+    else{
+        for(int i=qf;i<qb;++i){
+            cout<<qa[i%MAXN]<<" ";
+        }
+        cout<<endl;
     }
 }
 
 int main(){
-    freopen("queuesin.txt","r",stdin);
-    freopen("queuesout.txt","w",stdout);
-    queue<int> q;
-    int c;
-    scanf("%d",&c);
-    while (c){
-        if (c>0){
-        q.push(c);
-        print(q);
-        }
-        else {
-        q.pop();
-        print(q);
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    txtio("queuesin.txt","queuesout.txt");
+    int a;
+    while(true){
+        cin>>a;
+        if(a==-2)break;
+        else if(a==-1){int val=qpo();print();}
+        else{qpu(a);print();}
     }
-    scanf("%d",&c);
-    }
-    
 }
