e74cd08de46b4f5f57a4b35159251748e14f6cb9
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-07-23 21:12:35 +0800

    Update fib.cpp

--- AIO/fibonacci/fib.cpp
+++ AIO/fibonacci/fib.cpp
@@ -1,18 +1,78 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>; 
+using vs = vector<str>;
+using vc = vector<char>;
+using vvi = vector<vi>;
+using vvc = vector<vc>;
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define si set<int>
+#define sl set<ll>
+#define sb set<bool>
+#define sc set<char>
+#define ff first
+#define ss second
+#define sf scanf
+#define pf printf
+#define mii map<int,int>
+#define msi map<str,int>
+#define mis map<int,str>
+#define mci map<char,int>
+#define mdd map<double,double>
+#define mll map<ll,ll>
+#define pii pair<int,int>
+#define pis pair<int,str>
+#define psi pair<str,int>
+#define pss pair<str,str>
+#define pci pair<char,int>
+#define pcc pair<char,char>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define rep( i , s , e ) for(int i = s ; i <= e ; ++i)
+#define repR( i , e , s ) for(int i = e ; i >= s ; --i)
+#define dtest(x) cout<<x<<endl;
+#define arprint(x) for(auto ele:x){cout<<ele<<" ";}cout<<endl;
 
-int fib(int n) {
-    double phi = (1 + sqrt(5)) / 2;
-    return round(pow(phi, n) / sqrt(5));
+void inoutio(str s){
+    freopen((s + ".in").c_str(), "r" , stdin);
+    freopen((s + ".out")    .c_str(), "w" , stdout);
 }
- 
+
+void txtio(str inf,str ouf){
+    freopen(inf.c_str(), "r" , stdin);
+    freopen(ouf.c_str(), "w" , stdout);
+}
+
+const int MOD=1000;
+const int MAXN=1e6;
+
+pii fib(ll n) {
+  if (n == 0) return {0, 1};
+  auto p = fib(n >> 1);
+  ll c = p.ff * (2 * p.ss - p.ff);
+  ll d = p.ff * p.ff + p.ss * p.ss;
+  if (n & 1)
+    return {d%MOD, (c%MOD+d%MOD+MOD)%MOD};
+  else
+    return {c%MOD, d%MOD};
+}
+
 int main(){
-    freopen("fibin.txt","r",stdin);
-    freopen("fibout.txt","w",stdout);
-    int n,answer;
-    scanf("%d",&n);
-    answer=fib(n)%1000;
-    printf("%d",answer);
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    txtio("fibin.txt","fibout.txt");
+    ll n;
+    cin>>n;
+    pii ans=fib(n-1);
+    cout<<ans.ss<<endl;
 }
