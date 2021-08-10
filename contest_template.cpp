6396634134155fe4608eb3acfe123b6d94de2a50
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-08-10 09:03:37 +0800

    Add files via upload

---
+++ contest_template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using str = string;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define DEBUG
+
+const int MOD=1e9+7;
+const int MAXN=1e6;
+const int INF=INT_MAX;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+}
