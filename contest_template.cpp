eb13bb9cbe681648d41f979615dbe275707dd616
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-08-17 21:33:49 +0800

    Update contest_template.cpp

--- contest_template.cpp
+++ contest_template.cpp
@@ -8,7 +8,12 @@ const int MOD=1e9+7;
 const int MAXN=1e6;
 const int INF=INT_MAX;
 
+void solve();
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
+    solve();
 }
+
+void solve(){}
