0b4b97b33ed834ec6513cc1b8f57713c604df06e
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-08-19 16:59:54 +0800

    Add files via upload

---
+++ AIO/russian/dolls.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define DEBUG
+
+int n,x;
+vector<int> arr;
+
+void solve(){
+    cin >> n;
+	for(int i=0,x;i<n;i++){
+		cin >> x;
+		auto index=upper_bound(arr.begin(),arr.end(),x);
+		if(index==arr.end()) arr.push_back(x);
+		else arr[index-arr.begin()]=x;
+	}
+	cout << arr.size();
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    #ifndef DEBUG
+    freopen("dollin.txt","r",stdin);
+    freopen("dollout.txt","w",stdout);
+    #endif
+    solve();
+}
