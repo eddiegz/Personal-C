f7869826216e5c665f8f646502141f0dc680e545
eddiegz <61906782+eddiegz@users.noreply.github.com>
2021-08-25 20:15:06 +0800

    Update archery.py

--- AIO/Archery/archery.py
+++ AIO/Archery/archery.py
@@ -1,2 +1,1 @@
-n,a,b=map(int,open('archin.txt','r').readline().split())
-open('archout.txt','w').write(str(max(n-(n-a)-(n-b),1))+' '+str(min((a-1)+(b-1)+1,n)))
+n,a,b=map(int,open('archin.txt','r').readline().split());open('archout.txt','w').write(str(max(n-(n-a)-(n-b),1))+' '+str(min((a-1)+(b-1)+1,n)))
